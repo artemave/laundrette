81893d174bcee062e7bd44d13b8c732f8b7b0040
artemave <artemave@gmail.com>
2014-02-21 19:20:32 +0000

    sort and search on orders index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,8 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @search = Order.search(params[:q])
+    @orders = @search.result
   end
 
   # GET /orders/1
