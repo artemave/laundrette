30e01b7d8bc14a9bcc5e69b8c410826497c101f3
artemave <artemave@gmail.com>
2014-12-07 22:47:48 +0000

    add opbeat

---
+++ config/initializers/opbeat.rb
@@ -0,0 +1,5 @@
+Opbeat.configure do |config|
+ config.organization_id="02a4fef494a54229b238ecff53718b3f"
+ config.app_id="8f0dd786a8"
+ config.secret_token="ecb08ac7addf72b4f03382d1896d353ac7891d2c"
+end
